270e767ab5e263eaaa3cb8fdeb414ea2696f64d1
MaryAnnThomas <maryannthomas18@gmail.com>
2018-02-23 13:26:56 +0530

    Student ArrayList

---
+++ src/com/company/TestCollection3.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.*;
+
+class Student
+{
+    int rollno;
+    String name;
+    int age;
+    Student(int rollno,String name, int age)
+    {
+        this.rollno = rollno;
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
+public class TestCollection3 {
+    public static void main(String args[])
+    {
+      Student s1 = new Student(101,"Mary",25);
+      Student s2 = new Student(102,"Lettie",30);
+      Student s3 = new Student(103,"Glo",31);
+      ArrayList<Student> al = new ArrayList<Student>();
+      al.add(s1);
+      al.add(s2);
+      al.add(s3);
+      Iterator itr =al.iterator();
+      while(itr.hasNext())
+      {
+          Student st = (Student)itr.next();
+          System.out.println(st.rollno+" "+st.name+" "+st.age);
+      }
+    }
+}
