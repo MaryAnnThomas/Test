31e5dd677e428925184ab4ac672889d49a548c38
MaryAnnThomas <maryannthomas18@gmail.com>
2018-02-23 13:10:50 +0530

    ArrayList with Foreach Iteration

---
+++ src/com/company/TestCollection2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.*;
+
+public class TestCollection2 {
+    public static void main(String args[])
+    {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("Mary");
+        al.add("VIjay");
+        al.add("Lettie");
+        for(String obj:al)
+        {
+            System.out.println(obj);
+        }
+    }
+}
